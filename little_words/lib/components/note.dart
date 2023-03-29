e18f4cfcfb603466fc5608117574687f6e252160
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-29 16:48:01 +0200

    myChanges

---
+++ little_words/lib/components/note.dart
@@ -0,0 +1,33 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+
+class WordCard extends StatelessWidget {
+
+  final double latitude;
+  final double longitude;
+  final int id;
+
+  const WordCard({
+      super.key, 
+      required this.latitude, 
+      required this.longitude, 
+      required this.id}
+    );
+    
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding : const EdgeInsets.all(8.0),
+      child: Card(
+        color : Colors.white70,
+        child : Container(
+          padding: const EdgeInsets.all(15.0),
+          child: Text(
+            "#$id"
+          )
+        )
+      ),
+    );
+  }
+
+}
--- little_words/lib/main.dart
+++ little_words/lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'components/notes.dart';
+// import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+// import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,10 +24,20 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Little words'),
     );
   }
 }
+
+class MyAppState extends ChangeNotifier {
+  var current = 0;
+
+  void refreshList(){
+    notifyListeners();
+  }
+
+}
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -30,12 +49,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  List listeeee = [1,1,1];
+
+  Future fetchData(url) async {
+    final response = await http
+      .get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+        throw Exception('Failed to load album');
+      }
+  }
+
+  Future getAllItems() async {
+    LocationPermission permission;
+    bool  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    } 
+    var location = await Geolocator.getCurrentPosition();
+    // var url = "https://backend.smallwords.samyn.ovh/word/around?latitude=${location.latitude}&longitude=${location.longitude}";
+    var url = "https://backend.smallwords.samyn.ovh/word/around?latitude=50.950755&longitude=1.883361";
+    var data = await fetchData(url);
+    return data;
   }
 
   @override
@@ -46,24 +94,18 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        child:FutureBuilder(
+          future: getAllItems(),
+          builder: (context, snapshot){
+              if (snapshot.hasData){
+                var data = snapshot.data;
+                return WordCards(items: data['data']);
+              }
+              return const Row();
+            }
+          )
+      
+      )
     );
   }
 }
