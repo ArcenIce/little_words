af519af8534206b507d1dbcf0dd681508a85d9f1
Pierre Crepin <pierrot.859@gmail.com>
2023-03-30 10:50:58 +0200

    Update homepage.dart

--- lib/components/homepage.dart
+++ lib/components/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import '../components/notes.dart';
-
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:latlong2/latlong.dart';
 import '../helpers/dataHelper.dart';
 
 class MyAppState extends ChangeNotifier {
@@ -8,6 +9,12 @@ class MyAppState extends ChangeNotifier {
 
   void refreshList() {
     notifyListeners();
+  }
+}
+
+void setMarkers(data){
+  for (Map<String, dynamic> note in data){
+    print(note);
   }
 }
 
@@ -19,23 +26,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List listeeee = [1, 1, 1];
+  List<dynamic> locationNote = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: FutureBuilder(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [FutureBuilder(
                 future: getAllItems(),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    var data = snapshot.data;
-                    return WordCards(items: data['data']);
+                    locationNote = snapshot.data['data'];
+                    setMarkers(locationNote);
+                    return const Text("Chargé");
+                    //return WordCards(items: locationNote);
                   }
                   else {
                     return const Text("Chargement en cours");
                   }
-                  return const Row();
-                })));
+                }),
+                FutureBuilder(
+                future: getPosition(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Container(
+                        height: MediaQuery.of(context).size.height-80,
+                        alignment: Alignment.centerLeft,
+                        child: FlutterMap(
+                          options: MapOptions(
+                              center: LatLng(snapshot.data.latitude ?? 0, snapshot.data.longitude ?? 0),
+                              zoom: 17,
+                              maxZoom: 18,
+                          ),
+                          children: [
+                              TileLayer(
+                                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                                  userAgentPackageName: 'com.example.app',
+                              ),
+                              CurrentLocationLayer(),
+                          ],
+                        ),
+                      );
+                  }
+                  else {
+                    return const Text("");
+                  }
+                }),
+                ],
+              )
+            );
   }
 }
